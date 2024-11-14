4730a0878a00f416c0e1a06168ed4e1bd5a9c3df
mtane0412 <mtane0412@gmail.com>
2024-11-14 11:31:47 +0900

    🐛 (app/api/hello/route.ts): remove unnecessary request parameter from GET function

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,6 +1,6 @@
 export const dynamic = 'force-dynamic'; // static by default, unless reading the request
  
-export function GET(request: Request) {
+export function GET() {
   return new Response(`Hello from ${process.env.VERCEL_REGION}`);
 }
 export const runtime = 'nodejs';
